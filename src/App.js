27d596ee924809f3d3d9d514822e07db9d1eaed0
RexenN <96340446+RexenN@users.noreply.github.com>
2022-07-18 17:26:06 +0300

    button modal header

--- src/App.js
+++ src/App.js
@@ -15,14 +15,14 @@ import Footer from './components/footer/Footer';
 function App() {
   return (
     <BrowserRouter>
-        
+         <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path='/:category/search/:keyword' element={<Catalog />} />
       <Route path='/:category/:id' element={<Detail />} />
       <Route path='/:category' element={<Catalog />} />
     </Routes>
-      <Header />
+     
       <Footer />
   </BrowserRouter>
   );
