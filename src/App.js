e9da61f8acac2573f7e6fb81efdde59e9433c905
RexenN <96340446+RexenN@users.noreply.github.com>
2022-07-18 10:04:51 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 
+import 'swiper/swiper.min.css';
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
 import './App.scss';
+import Catalog from './pages/Catalog';
+import Detail from './pages/Detail';
+import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
+
+
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+        
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path='/:category/search/:keyword' element={<Catalog />} />
+      <Route path='/:category/:id' element={<Detail />} />
+      <Route path='/:category' element={<Catalog />} />
+    </Routes>
+      <Header />
+      <Footer />
+  </BrowserRouter>
   );
 }
 
