fcdc7875f2c0d9e23783911a16a3b8c878bf804d
RexenN <96340446+RexenN@users.noreply.github.com>
2022-07-19 16:36:21 +0300

    trying to solve error

--- src/api/tmdbApi.js
+++ src/api/tmdbApi.js
@@ -2,32 +2,31 @@ import axiosClient from "./axiosClient";
 
 export const category = {
     movie: 'movie',
-    tv: 'tv',
+    tv: 'tv'
 }
-
 
 export const movieType = {
     upcoming: 'upcoming',
     popular: 'popular',
-    top_rated: 'top_rated',
+    top_rated: 'top_rated'
 }
 
 export const tvType = {
-    on_the_air: 'on_the_air',
     popular: 'popular',
     top_rated: 'top_rated',
+    on_the_air: 'on_the_air'
 }
 
 const tmdbApi = {
-    getMoviesList: (type, params)  => {
+    getMoviesList: (type, params) => {
         const url = 'movie/' + movieType[type];
         return axiosClient.get(url, params);
     },
-    getTvList: (type, params)  => {
-        const url = 'movie/' + movieType[type];
+    getTvList: (type, params) => {
+        const url = 'tv/' + tvType[type];
         return axiosClient.get(url, params);
     },
-    getVideos: (cate, id)  => {
+    getVideos: (cate, id) => {
         const url = category[cate] + '/' + id + '/videos';
         return axiosClient.get(url, {params: {}});
     },
--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types'
 import './modal.scss';
 
